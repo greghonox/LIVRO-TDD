00b08a7f4f8bdaf0b7e4d56354da5a6303cef845
Gregorio Honorato <greghono@gmail.com>
2021-04-06 22:04:40 -0300

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -22,7 +22,12 @@ class ItemModel(TestCase):
         self.assertEqual(first_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
         
+        
 class HomePageTest(TestCase):
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+    
     def test_root_url_page(self):
         root = resolve('/')
         self.assertEqual(root.func, home_page)
@@ -33,6 +38,24 @@ class HomePageTest(TestCase):
         
     def test_can_save_a_post_request(self):
         txt = 'A new list item'
-        response = self.client.get('/', data={'item': txt})
-        self.assertIn(txt, response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.client.post('/', data={'item_text': txt})
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, txt)
+        
+    def test_redirect_after_post(self):
+        txt = 'A new list item'
+        response = self.client.post('/', data={'item_text': txt})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+    
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('/')
+        
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+        
